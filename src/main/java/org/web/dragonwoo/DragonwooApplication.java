df5b3c256c7eef22f4bce58d835300fbbb173540
dragonwoo <mire2566@naver.com>
2024-07-26 10:29:42 +0900

    백엔드 초기 환경설정 완료

---
+++ src/main/java/org/web/dragonwoo/DragonwooApplication.java
@@ -0,0 +1,13 @@
+package org.web.dragonwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonwooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonwooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/web/dragonwoo/DragonwooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.web.dragonwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonwooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
